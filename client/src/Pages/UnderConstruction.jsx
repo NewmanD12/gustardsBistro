aeaba3e639baa49c1eedd87d0433d0b046ddc1d2
Dakota Newman <dakota.newman@yahoo.com>
2024-09-29 20:31:22 -0400

    changed verbage on tasting room page

--- client/src/Pages/UnderConstruction.jsx
+++ client/src/Pages/UnderConstruction.jsx
@@ -5,11 +5,16 @@ import './UnderConstruction.css'
 import React from 'react'
 
 const UnderConstruction = () => {
+
+  const CallTo = ({phone, children}) => {
+    return <a href={`tel:${phone}`} id='phoneNumber'>{children}</a>;
+  }
+
   return (
     <Container fluid id='under-construction-body'>
         <Row className='justify-content-center text-center mx-5'>
             <Col md={8}>
-                <h1>The Tasting Room will open it's doors June 1st as we work on a fun and creative menu. We look forward to serving you with our upscale dining experience.</h1>
+                <h1>The Tasting Room is a great place to have small intimate parties and dinners. Please give us a call at <CallTo phone="+13045362274">{`(304) 536-2274`}</CallTo> or email us at gustardsbistro@gmail.com if you would like to book an event.</h1>
             </Col>
         </Row>
     </Container>
